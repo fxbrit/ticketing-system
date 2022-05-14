49b414fca4a4a9ddc4af7581e66ee647dc2e8cb5
Ale142 <ale.bacci142@gmail.com>
2022-05-14 13:45:39 +0200

    first commit

---
+++ src/main/kotlin/com/example/group03reactivekafka/Group03ReactiveKafkaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.group03reactivekafka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Group03ReactiveKafkaApplication
+
+fun main(args: Array<String>) {
+    runApplication<Group03ReactiveKafkaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/group03reactivekafka/Group03ReactiveKafkaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.group03reactivekafka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Group03ReactiveKafkaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
