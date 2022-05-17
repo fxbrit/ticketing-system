95e777a44bbb41e1ba020f8e81fb0f85e742545f
Ale142 <ale.bacci142@gmail.com>
2022-05-17 18:15:07 +0200

    added dependency for postgresql r2dbc

--- TicketCatalogueService/build.gradle.kts
+++ TicketCatalogueService/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.r2dbc:r2dbc-postgresql")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework.kafka:spring-kafka")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
