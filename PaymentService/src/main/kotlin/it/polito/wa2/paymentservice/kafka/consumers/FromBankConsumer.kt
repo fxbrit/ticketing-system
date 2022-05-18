63f4ee551a6191f87a6db973171207f2023cfd9f
fxbrit <fxbrit@tuta.io>
2022-05-18 15:54:41 +0200

    consumers: add FromBankConsumer.kt, FromTicketCatalogueConsumer.kt

---
+++ PaymentService/src/main/kotlin/it/polito/wa2/paymentservice/kafka/consumers/FromBankConsumer.kt
@@ -0,0 +1,18 @@
+package it.polito.wa2.paymentservice.kafka.consumers
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+
+class FromBankConsumer {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @KafkaListener(topics = ["fromBank"], groupId = "ppr")
+    fun listenFromBank(consumerRecord: ConsumerRecord<Any, Any>, ack: Acknowledgment) {
+        logger.info("Message received {}", consumerRecord)
+        ack.acknowledge()
+    }
+
+}
---
+++ PaymentService/src/main/kotlin/it/polito/wa2/paymentservice/kafka/consumers/FromTicketCatalogueConsumer.kt
@@ -0,0 +1,20 @@
+package it.polito.wa2.paymentservice.kafka.consumers
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+
+@Component
+class FromTicketCatalogueConsumer {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @KafkaListener(topics = ["fromTicketCatalogue"], groupId = "ppr")
+    fun listenFromTicketCatalogue(consumerRecord: ConsumerRecord<Any, Any>, ack: Acknowledgment) {
+        logger.info("Message received {}", consumerRecord)
+        ack.acknowledge()
+    }
+
+}
