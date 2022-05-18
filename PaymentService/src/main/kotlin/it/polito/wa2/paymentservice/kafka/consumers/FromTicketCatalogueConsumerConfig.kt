50f56ef5de845a9e3b662f3fd044f69cd74f1d27
fxbrit <fxbrit@tuta.io>
2022-05-18 15:54:00 +0200

    consumers: add configurations

---
+++ PaymentService/src/main/kotlin/it/polito/wa2/paymentservice/kafka/consumers/FromTicketCatalogueConsumerConfig.kt
@@ -0,0 +1,35 @@
+package it.polito.wa2.paymentservice.kafka.consumers
+
+import it.polito.wa2.paymentservice.kafka.serializers.PaymentRequestDeserializer
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.listener.ContainerProperties
+
+class FromTicketCatalogueConsumerConfig(@Value("\${spring.kafka.bootstrap-servers}") private val server: String) {
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String?, Any?> {
+        val props: MutableMap<String, Any> = HashMap()
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = server
+        props[ConsumerConfig.GROUP_ID_CONFIG] = "ppr"
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = PaymentRequestDeserializer::class.java
+        props[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = consumerFactory()
+        factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
+        factory.containerProperties.isSyncCommits = true
+        return factory
+    }
+
+}
