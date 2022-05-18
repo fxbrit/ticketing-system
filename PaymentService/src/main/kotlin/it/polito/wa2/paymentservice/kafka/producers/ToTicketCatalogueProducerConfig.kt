d7347772a3e244a8e99188976756490e5a9651a0
fxbrit <fxbrit@tuta.io>
2022-05-18 15:55:26 +0200

    producers: add configurations

---
+++ PaymentService/src/main/kotlin/it/polito/wa2/paymentservice/kafka/producers/ToTicketCatalogueProducerConfig.kt
@@ -0,0 +1,28 @@
+package it.polito.wa2.paymentservice.kafka.producers
+
+import it.polito.wa2.paymentservice.kafka.serializers.PaymentResponseSerializer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+class ToTicketCatalogueProducerConfig(@Value("\${spring.kafka.bootstrap-servers}") private val server: String) {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, Any> {
+        val configProps: MutableMap<String, Any> = HashMap()
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = server
+        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = PaymentResponseSerializer::class.java
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, Any> {
+        return KafkaTemplate(producerFactory())
+    }
+
+}
