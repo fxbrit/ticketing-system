998a3d04e8fb3143760d4ca6fb896634be347e94
fxbrit <fxbrit@tuta.io>
2022-05-19 19:39:40 +0200

    controllers: improve TestingController.kt to mock Bank and TicketCatalogue

--- PaymentService/src/main/kotlin/it/polito/wa2/paymentservice/controller/TestingController.kt
+++ PaymentService/src/main/kotlin/it/polito/wa2/paymentservice/controller/TestingController.kt
@@ -1,5 +1,6 @@
 package it.polito.wa2.paymentservice.controller
 
+import it.polito.wa2.paymentservice.entities.PaymentRequest
 import it.polito.wa2.paymentservice.entities.PaymentResponse
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,38 +19,68 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestingController(
-    @Value("toTicketCatalogue") val topic: String,
+    @Value("fromBank") val topicFB: String,
+    @Value("fromTicketCatalogue") val topicFTC: String,
     @Autowired
     @Qualifier("toTicketCatalogueTemplate")
-    private val kafkaTemplate: KafkaTemplate<String, Any>
+    private val ticketCatalogueTemplate: KafkaTemplate<String, Any>,
+    @Autowired
+    @Qualifier("toBankTemplate")
+    private val bankTemplate: KafkaTemplate<String, Any>
 ) {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
 
     /**
-     * testing route, simulate receiving a response from the Bank:
-     * curl -H "Content-Type: application/json" -d '{"paymentId":10, "status":1)' localhost:8080/tests/reponseFromBank
+     * simulates the Bank publishing a response:
+     * curl -H "Content-Type: application/json" -d '{"paymentId":10, "status":1}' localhost:8080/tests/reponseFromBank
      *
      * expected behavior:
-     *  - message is serialized
-     *  - message is sent to TicketCatalogueService
-     *
-     * missing:
-     *  - it should also save to DB
-     *  - it should be implemented in the consumer that listens to the bank
+     *  - message is deserialized
+     *  - DB is updated with response status
+     *  - message is forwarded to outgoing topic
      */
     @PostMapping("/tests/reponseFromBank")
-    fun post(@Validated @RequestBody paymentResponse: PaymentResponse): ResponseEntity<Any> {
+    fun reponseFromBank(@Validated @RequestBody paymentResponse: PaymentResponse): ResponseEntity<Any> {
         return try {
-            log.info("Sending PaymentResponse to TicketCatalogueService")
-            log.info("Sending message to Kafka that looks like: {}", paymentResponse)
+            log.info("New message from the Bank")
+            log.info("Bank sending message to Kafka that looks like: {}", paymentResponse)
             val message: Message<PaymentResponse> = MessageBuilder
                 .withPayload(paymentResponse)
-                .setHeader(KafkaHeaders.TOPIC, topic)
+                .setHeader(KafkaHeaders.TOPIC, topicFB)
                 .build()
-            kafkaTemplate.send(message)
-            log.info("Message sent with success")
+            ticketCatalogueTemplate.send(message)
+            log.info("Message from Bank sent with success")
+            ResponseEntity.ok().build()
+        } catch (e: Exception) {
+            log.error("Exception: {}", e)
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error to send message")
+        }
+    }
+
+    /**
+     * simulates the TicketCatalogueService publishing a request:
+     * curl -H "Content-Type: application/json" \
+     *      -d '{"paymentId":1, "userId":2, "creditCardNumber":333, "cvv":445, "expirationDate":"2022-05-25", "amount":10}' \
+     *      localhost:8080/tests/requestTicketCatalogue
+     *
+     * expected behavior:
+     *  - message is deserialized
+     *  - (paymentid,userid,0) is saved to DB, to indicate the pending transaction
+     *  - message is forwarded to outgoing topic, towards Bank
+     */
+    @PostMapping("/tests/requestTicketCatalogue")
+    fun requestTicketCatalogue(@Validated @RequestBody paymentRequest: PaymentRequest): ResponseEntity<Any> {
+        return try {
+            log.info("New message from the TicketCatalogue")
+            log.info("TicketCatalogue sending message to Kafka that looks like: {}", paymentRequest)
+            val message: Message<PaymentRequest> = MessageBuilder
+                .withPayload(paymentRequest)
+                .setHeader(KafkaHeaders.TOPIC, topicFTC)
+                .build()
+            bankTemplate.send(message)
+            log.info("Message from TicketCatalogue sent with success")
             ResponseEntity.ok().build()
         } catch (e: Exception) {
             log.error("Exception: {}", e)
