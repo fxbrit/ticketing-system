287cd487595566471c15e0ee2703418c4e5690dd
Luca Errani <luca.errani1@gmail.com>
2022-05-19 11:35:48 +0200

    Fixes #2 #6 - Get and post tickets to ticket catalogue

--- TicketCatalogueService/src/main/kotlin/it/polito/wa2/ticketcatalogueservice/Controllers/TicketCatalogueController.kt
+++ TicketCatalogueService/src/main/kotlin/it/polito/wa2/ticketcatalogueservice/Controllers/TicketCatalogueController.kt
@@ -1,10 +1,13 @@
 package it.polito.wa2.ticketcatalogueservice.Controllers
 
 import it.polito.wa2.ticketcatalogueservice.Entities.Order
+import it.polito.wa2.ticketcatalogueservice.Entities.Ticket
 import it.polito.wa2.ticketcatalogueservice.Services.TicketCatalogueService
 import kotlinx.coroutines.flow.Flow
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,5 +18,15 @@ class TicketCatalogueController {
     @GetMapping("/admin/orders")
     fun getAllOrders(): Flow<Order> {
         return ticketCatalogueService.getAllOrders()
+    }
+
+    @GetMapping("/tickets")
+    fun getAllTickets(): Flow<Ticket> {
+        return ticketCatalogueService.getAllTickets()
+    }
+
+    @PostMapping("/admin/tickets")
+    suspend fun addNewTicket(@RequestBody newTicket: Ticket): Flow<Ticket> {
+        return ticketCatalogueService.createNewTicket(newTicket)
     }
 }
