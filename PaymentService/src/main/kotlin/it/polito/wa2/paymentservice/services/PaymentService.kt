34f39e74c90180c35df231024daa488d54e6aea4
fxbrit <c53j0zo1y@mozmail.com>
2022-07-15 15:12:02 +0200

    PaymentService: return DTOs in APIs

--- PaymentService/src/main/kotlin/it/polito/wa2/paymentservice/services/PaymentService.kt
+++ PaymentService/src/main/kotlin/it/polito/wa2/paymentservice/services/PaymentService.kt
@@ -1,8 +1,10 @@
 package it.polito.wa2.paymentservice.services
 
-import it.polito.wa2.paymentservice.entities.Payment
+import it.polito.wa2.paymentservice.dtos.PaymentDTO
+import it.polito.wa2.paymentservice.dtos.toDTO
 import it.polito.wa2.paymentservice.repositories.PaymentRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -12,12 +14,12 @@ class PaymentService {
     @Autowired
     lateinit var paymentRepository: PaymentRepository
 
-    fun getAllPayments(): Flow<Payment> {
-        return paymentRepository.findAll()
+    fun getAllPayments(): Flow<PaymentDTO> {
+        return paymentRepository.findAll().map { it.toDTO() }
     }
 
-    suspend fun getAllPaymentsByUser(userId: Long): Flow<Payment> {
-        return paymentRepository.findAllByUserId(userId)
+    suspend fun getAllPaymentsByUser(userId: Long): Flow<PaymentDTO> {
+        return paymentRepository.findAllByUserId(userId).map { it.toDTO() }
     }
 
 }
