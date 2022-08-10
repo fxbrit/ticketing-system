bac118801e180ce069264d229242907d7e34b6d1
fxbrit <34379516+fxbrit@users.noreply.github.com>
2022-08-10 17:31:16 +0200

    fix bugs that appeared because of rebase

--- TravelerService/src/main/kotlin/it/polito/wa2/group03authenticationauthorization/controllers/TicketsController.kt
+++ TravelerService/src/main/kotlin/it/polito/wa2/group03authenticationauthorization/controllers/TicketsController.kt
@@ -103,40 +103,6 @@ class TicketsController {
 
     }
 
-    @GetMapping(value = ["/my/tickets/{ticketId}"], produces = [MediaType.IMAGE_PNG_VALUE])
-    fun getTicketQRCode(@PathVariable ticketId: UUID): ResponseEntity<ByteArray> {
-
-        val ticket = try {
-            ticketsService.getTicket(ticketId).jws!!
-        } catch (ex: Exception) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build()
-        }
-
-        val qr = QRCode(ticket).render(8, 20)
-        val ba = ByteArrayOutputStream()
-            .also { qr.writeImage(it) }
-            .toByteArray()
-
-        return ResponseEntity.status(HttpStatus.OK).body(ba)
-    }
-
-    @GetMapping(value = ["/my/tickets/{ticketId}"], produces = [MediaType.IMAGE_PNG_VALUE])
-    fun getTicketQRCode(@PathVariable ticketId: UUID): ResponseEntity<ByteArray> {
-
-        val ticket = try {
-            ticketsService.getTicket(ticketId).jws!!
-        } catch (ex: Exception) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build()
-        }
-
-        val qr = QRCode(ticket).render(8, 20)
-        val ba = ByteArrayOutputStream()
-            .also { qr.writeImage(it) }
-            .toByteArray()
-
-        return ResponseEntity.status(HttpStatus.OK).body(ba)
-    }
-
     @GetMapping("/admin/travelers")
     fun getTravelersUsernames(): List<String> {
         return userDetailsService.getRegisteredUsernames()
